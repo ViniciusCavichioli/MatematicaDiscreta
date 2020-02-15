f8bd16199aee35d2bafdcb29c222203c01077650
Vinicius <vinicavi@hotmail.com>
2020-02-14 23:47:45 -0300

    Aula2

--- Conjuntos/Conjunto.py
+++ Conjuntos/Conjunto.py
@@ -9,17 +9,39 @@ class Conjunto():
         self.nome = nome
 
     def adicionar_Elemento(self,item):
-        if item not in self.elementos:
+        if isinstance(item, Conjunto):
             self.elementos.append(item)
-            self.tamanho += 1
-            
-    def imprimir(self):
-        result = ""
-        verifica = self.elementos[-1]
-        for i in self.elementos:
-            if i != verifica:
-                result += str(i) + ","
-            else:
-                result += str(i)
+        else:
+            if item not in self.elementos:
+                self.elementos.append(item)
+                self.tamanho += 1
 
-        print(self.nome, "= " "{",result,"}")
+
+    def __str__(self):
+        return '{' + ', '.join([str(i) for i in self.elementos]) + '}'
+
+    
+    def imprimir(self):
+        print(self.nome, "=", self)
+        
+    def pertence(self,elemento):
+        if elemento in self.elementos:
+            return True
+        else:
+            return False
+    
+    def eh_subconjunto(self,conjunto):
+        for i in conjunto:
+            if i not in self.elementos:
+                return False
+        return True
+    
+    def eh_subconjunto_proprio(self,conjunto):
+        for i in conjunto:
+            if i not in self.elementos:
+                return False
+        return True
+        for i in self.elementos:
+            if i not in conjunto:
+                return True
+        return False
--- Conjuntos/Teste.py
+++ Conjuntos/Teste.py
@@ -1,9 +1,24 @@
 import Conjunto
 
+
+
+A = Conjunto.Conjunto()
+A.nome_Conjunto("A")
+A.adicionar_Elemento(1)
+A.adicionar_Elemento(2)
+A.adicionar_Elemento(3)
+
+
+B = Conjunto.Conjunto()
+B.nome_Conjunto("B")
+B.adicionar_Elemento(0)
+
+
+
 c = Conjunto.Conjunto()
 
-c.nome_Conjunto("B")
-
+c.nome_Conjunto("C")
+c.adicionar_Elemento(1)
 c.adicionar_Elemento(2)
 c.adicionar_Elemento(2)
 c.adicionar_Elemento(2)
@@ -13,4 +28,22 @@ c.adicionar_Elemento(3)
 c.adicionar_Elemento(11)
 c.adicionar_Elemento(3)
 c.adicionar_Elemento(9)
+c.adicionar_Elemento(A)
+c.adicionar_Elemento(B)
+
+print("Listas:")
+A.imprimir()
+B.imprimir()
 c.imprimir()
+print()
+
+
+print("1 pertence a C? ",c.pertence(1))
+print("A é subconjunto de C ? ",c.eh_subconjunto(A.elementos))
+print("B é subconjunto de C? ",c.eh_subconjunto(B.elementos))
+print()
+print("A é subconjunto proprio de C? ", c.eh_subconjunto_proprio(A.elementos))
+print("B é subconjunto proprio de C? ", c.eh_subconjunto_proprio(B.elementos))
+
+
+
