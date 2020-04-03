b0fa77ed2cbcbb765f57820598de4260b7d6d461
Vinicius <vinicavi@hotmail.com>
2020-04-03 18:27:56 -0300

    Final

--- Conjuntos/Conjunto.py
+++ Conjuntos/Conjunto.py
@@ -1,29 +1,25 @@
+
+import itertools
 class Conjunto():
-
-    def __init__ (self):
+    def __init__ (self, nome):
         self.elementos = []
-        self.tamanho = 0
-        self.nome = None
-
-    def nome_Conjunto(self,nome):
         self.nome = nome
-
+        
     def adicionar_Elemento(self,item):
         if isinstance(item, Conjunto):
             self.elementos.append(item)
         else:
             if item not in self.elementos:
                 self.elementos.append(item)
-                self.tamanho += 1
-
-
+                
     def __str__(self):
         return '{' + ', '.join([str(i) for i in self.elementos]) + '}'
 
     
     def imprimir(self):
         print(self.nome, "=", self)
-        
+
+
     def pertence(self,elemento):
         if elemento in self.elementos:
             return True
@@ -31,17 +27,128 @@ class Conjunto():
             return False
     
     def eh_subconjunto(self,conjunto):
-        for i in conjunto:
-            if i not in self.elementos:
-                return False
+        if (type(conjunto) == Conjunto):    
+            for i in conjunto.elementos:
+                if i not in self.elementos:
+                    return False
+            return True
+
+    def eh_subconjunto_proprio(self,conjunto):
+        if not self.eh_subconjunto(conjunto):
+            return False
         return True
     
-    def eh_subconjunto_proprio(self,conjunto):
-        for i in conjunto:
-            if i not in self.elementos:
-                return False
-        return True
+    def verifica(self,conjunto):
+        if (type(conjunto) == Conjunto):
+            return True
+        
+    def ElementoNeutroE(self,conjunto):
+        if len(self.elementos) == 0 and len(conjunto.elementos) != 0:
+            return True
+        else:
+            return False
+        
+    def ElementoNeutroD(self,conjunto):
+        if len(conjunto.elementos) == 0 and len(self.elementos) != 0:
+            return True
+        else:
+            return False
+        
+    def Idempotencia(self,conjunto):
+        if self.elementos == conjunto.elementos:
+            return True
+        else:
+            return False
+        
+    def RealizaUniao(self, conjunto):
+        conjuntoResultado = []
         for i in self.elementos:
-            if i not in conjunto:
-                return True
-        return False
+            conjuntoResultado.append(i)
+        for i in conjunto.elementos:
+            if i not in conjuntoResultado:
+                conjuntoResultado.append(i)
+        return conjuntoResultado
+       
+    def uniao(self,conjunto):
+        if self.ElementoNeutroE(conjunto) == True:
+            return print(self.nome,'U',conjunto.nome,'=',self.nome)
+        elif self.ElementoNeutroD(conjunto) == True:
+            return print(self.nome,'U',conjunto.nome,'=',conjunto.nome)
+        elif self.Idempotencia(conjunto) == True:
+            return print(self.nome,'U',self.nome,'=',conjunto.nome)
+        else:
+            result = ""
+            verifica = self.RealizaUniao(conjunto)[-1]
+            for i in self.RealizaUniao(conjunto):
+                if i != verifica:
+                    result += str(i) + ","
+                else:
+                    result += str(i)
+            return print(self.nome,"U",conjunto.nome,"= " "{",result,"}")
+
+    def ElementoNeutroI(self,conjunto):
+        conjuntoResultado = []
+        if conjunto.nome == 'U' or self.nome == 'U':
+            for i in self.elementos:
+                if i in conjunto.elementos:
+                    conjuntoResultado.append(i)
+        else:
+            return False
+        
+        if len(conjuntoResultado) == 0:
+            return False
+        return True
+            
+    def RealizaInterseccao(self, conjunto):
+        conjuntoResultado = []
+        for i in self.elementos:
+            if i in conjunto.elementos:
+                conjuntoResultado.append(i)
+        return conjuntoResultado
+
+    def Interseccao(self,conjunto):
+        if self.ElementoNeutroI(conjunto) == True:
+            if self.nome == 'U':
+                return print(self.nome,'∩',conjunto.nome,'=',conjunto.nome)
+            else:
+                return print(self.nome,'∩',conjunto.nome,'=',self.nome)
+        if self.Idempotencia(conjunto) == True:
+            return print(self.nome,'∩',self.nome,'=',conjunto.nome)
+        else:
+            result = ""
+            try:
+                verifica = self.RealizaInterseccao(conjunto)[-1]
+            except:
+                print('')
+            for i in self.RealizaInterseccao(conjunto):
+                if i != verifica:
+                    result += str(i) + ","
+                else:
+                    result += str(i)
+            return print(self.nome,"∩",conjunto.nome,"= " "{",result,"}")
+
+
+    
+    def Complemento (self,conjunto):
+        conjuntoResultado = []
+        for i in self.elementos:
+            if i not in conjunto.elementos:
+                conjuntoResultado.append(i)
+        return conjuntoResultado
+
+    def Diferenca(self, conjunto):
+        conjuntoResultado = []
+        for i in self.elementos:
+            if i not in conjunto.elementos:
+                conjuntoResultado.append(i)
+        return conjuntoResultado
+
+    def conjuntoPartes(self):
+        conjuntoResult = []
+        tamanhoConjunto = len(self.elementos)
+        for x in range(tamanhoConjunto):
+            for i in itertools.combinations(self.elementos, x):
+                conjuntoResult.append(i)
+        return conjuntoResult
+
+
--- Conjuntos/Teste.py
+++ Conjuntos/Teste.py
@@ -2,48 +2,82 @@ import Conjunto
 
 
 
-A = Conjunto.Conjunto()
-A.nome_Conjunto("A")
+A = Conjunto.Conjunto("A")
 A.adicionar_Elemento(1)
 A.adicionar_Elemento(2)
 A.adicionar_Elemento(3)
-
-
-B = Conjunto.Conjunto()
-B.nome_Conjunto("B")
+#----------------------------------
+B = Conjunto.Conjunto("B")
 B.adicionar_Elemento(0)
-
-
-
-c = Conjunto.Conjunto()
-
-c.nome_Conjunto("C")
-c.adicionar_Elemento(1)
-c.adicionar_Elemento(2)
-c.adicionar_Elemento(2)
-c.adicionar_Elemento(2)
-c.adicionar_Elemento(5)
-c.adicionar_Elemento(10)
-c.adicionar_Elemento(3)
-c.adicionar_Elemento(11)
-c.adicionar_Elemento(3)
-c.adicionar_Elemento(9)
-c.adicionar_Elemento(A)
-c.adicionar_Elemento(B)
+#----------------------------------
+C = Conjunto.Conjunto("C")
+C.adicionar_Elemento(1)
+C.adicionar_Elemento(2)
+C.adicionar_Elemento(2)
+C.adicionar_Elemento(2)
+C.adicionar_Elemento(5)
+C.adicionar_Elemento(10)
+C.adicionar_Elemento(3)
+C.adicionar_Elemento(11)
+C.adicionar_Elemento(3)
+C.adicionar_Elemento(9)
+C.adicionar_Elemento(A)
+C.adicionar_Elemento(B)
+#----------------------------------
+D = Conjunto.Conjunto("D")
+#----------------------------------
+U = Conjunto.Conjunto("U")
+U.adicionar_Elemento(0)
+U.adicionar_Elemento(1)
+U.adicionar_Elemento(2)
+U.adicionar_Elemento(3)
+U.adicionar_Elemento(5)
+U.adicionar_Elemento(9)
+U.adicionar_Elemento(10)
+U.adicionar_Elemento(11)
+#----------------------------------
+E = Conjunto.Conjunto("E")
+E.adicionar_Elemento(5)
+E.adicionar_Elemento(9)
+E.adicionar_Elemento(10)
+E.adicionar_Elemento(11)
 
 print("Listas:")
 A.imprimir()
 B.imprimir()
-c.imprimir()
+C.imprimir()
+D.imprimir()
+U.imprimir()
 print()
 
-
-print("1 pertence a C? ",c.pertence(1))
-print("A é subconjunto de C ? ",c.eh_subconjunto(A.elementos))
-print("B é subconjunto de C? ",c.eh_subconjunto(B.elementos))
+print("1 pertence a C? ",C.pertence(1))
+print("A é subconjunto de C ? ",C.eh_subconjunto(A))
+print("B é subconjunto de C? ",C.eh_subconjunto(B))
 print()
-print("A é subconjunto proprio de C? ", c.eh_subconjunto_proprio(A.elementos))
-print("B é subconjunto proprio de C? ", c.eh_subconjunto_proprio(B.elementos))
+print("A é subconjunto proprio de C? ", C.eh_subconjunto_proprio(A))
+print("B é subconjunto proprio de C? ", C.eh_subconjunto_proprio(B))
 
-
+def imprimir2(metodo,nome):
+    result = ""
+    verifica = metodo[-1]
+    for i in metodo:
+        if i != verifica:
+            result += str(i) + ","
+        else:
+            result += str(i)
+    print(nome, "= " "{",result,"}")
+    
+imprimir2(U.Complemento(B),'Complemento de B')
+imprimir2(U.Diferenca(A),'Diferença de A')
+imprimir2(A.conjuntoPartes(),'ConjuntoParte de A')
+A.uniao(D)
+A.uniao(B)
+A.uniao(A)
+A.Interseccao(U)
+U.Interseccao(A)
+U.Interseccao(U)
+A.Interseccao(E)
+C.Interseccao(A)
+A.Interseccao(A)
+C.Interseccao(A)
 
